598dfd81610b9201088f635f073a4e066fdfd5e7
Nuria <c.odena.nuria@gmail.com>
2020-05-16 21:00:13 +0200

    Other Ways to Query the DOM

--- CHAPTER 5. The Document Object Model/dom.js
+++ CHAPTER 5. The Document Object Model/dom.js
@@ -16,8 +16,6 @@ paras.forEach(para => {
     console.log(para)
 })
 
-console.log(paras)
-
 // Get an element by ID 
 
 const title = document.getElementById('page-title');
@@ -30,4 +28,37 @@ const errors = document.getElementsByClassName('error');
 
 const paragraphs = document.getElementsByTagName('p');
 
+// Adding & Changing Page content
 
+const pTag = document.querySelector('p');
+
+// innerText is a property that gets the inner text of an element
+
+// By using = we get rid of the previous text and it is overwritten
+
+pTag.innerText = 'I am changing the text';
+
+// In case we just want to append to the previous, we use +=
+
+pTag.innerText += 'I am changing the text';
+
+// Updating the content of multiple elements
+
+const moreParas = document.querySelectorAll('p');
+
+paras.forEach(para => {
+    console.log(para.innerText);
+    para.innerText += 'adding more text'
+})
+
+// Updating the template by using innerHTML
+
+const content = document.querySelector('.content');
+
+content.innerHTML = '<h2>This is a new h2</h2>';
+
+const people = ['Marc', 'John', 'Shaun', 'Brad'];
+
+people.forEach(person => {
+    content.innerHTML += `<p>${person}</p>`
+});
