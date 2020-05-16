e1327706b4692bd67433a724f86217d857f79fbf
Nuria <c.odena.nuria@gmail.com>
2020-05-16 20:29:30 +0200

    Other Ways to Query the DOM

--- CHAPTER 5. The Document Object Model/dom.js
+++ CHAPTER 5. The Document Object Model/dom.js
@@ -9,8 +9,25 @@ console.log(para)
 
 const paras = document.querySelectorAll('p');
 
+const groupErrors = document.querySelectorAll('.error');
+
+
 paras.forEach(para => {
     console.log(para)
 })
 
 console.log(paras)
+
+// Get an element by ID 
+
+const title = document.getElementById('page-title');
+
+// Get elements by their class name
+
+const errors = document.getElementsByClassName('error');
+
+// Get elements by their tag name
+
+const paragraphs = document.getElementsByTagName('p');
+
+
