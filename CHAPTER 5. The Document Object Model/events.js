5da2231d93a0a2e3d81ce8631a3c00b7efd7bb6c
Nuria <c.odena.nuria@gmail.com>
2020-05-18 17:48:53 +0200

    More DOM Events

--- CHAPTER 5. The Document Object Model/events.js
+++ CHAPTER 5. The Document Object Model/events.js
@@ -68,3 +68,26 @@ ul.addEventListener('click', e => {
     }
 
 })
+
+// More DOM Events
+
+// Copy event
+
+const copy = document.querySelector('.copy-me');
+
+copy.addEventListener('copy', () => {
+    console.log('copied')
+})
+
+// Mouse move event
+
+const box = document.querySelector('.box');
+
+box.addEventListener('mousemove', e => {
+// console.log(e.offsetX, e.offsetY)
+box.textContent = `x pos - ${e.offsetX} y pos - ${e.offsetY} `
+});
+
+document.addEventListener('wheel', (e) => {
+    console.log(e.pageX, e.pageY)
+})
